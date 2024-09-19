533d161bc0c3c94fd7e20820e0a9af5df4b7edff
Oludowole Olumide <xeonncodes@gmail.com>
2024-09-19 16:01:33 +0100

    Updated

--- layouts/head.tsx
+++ layouts/head.tsx
@@ -10,11 +10,14 @@ export const Head = () => {
       <meta key="title" content={siteConfig.name} property="og:title" />
       <meta content={siteConfig.description} property="og:description" />
       <meta content={siteConfig.description} name="description" />
+
+      {/* Updated viewport meta tag */}
       <meta
         key="viewport"
-        content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, overflow=hidden"
         name="viewport"
       />
+
       <link href="/favicon.ico" rel="icon" />
     </NextHead>
   );
