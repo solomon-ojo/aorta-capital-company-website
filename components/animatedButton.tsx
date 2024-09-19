40592ca915effa01c264b95e8017342cd0e95cad
Oludowole Olumide <xeonncodes@gmail.com>
2024-09-19 14:53:00 +0100

    Updated

--- components/animatedButton.tsx
+++ components/animatedButton.tsx
@@ -1,6 +1,6 @@
 export default function AnimatedButton() {
   return (
-    <button className="relative group border-1 border-[#6E6F7A] w-full text-black md:w-[160px] h-[45px] font-semibold overflow-hidden transition-all duration-300">
+    <button className="relative home-button group border-1 border-[#6E6F7A] w-full text-black md:w-[160px] h-[45px] font-semibold overflow-hidden transition-all duration-300">
       {/* Background layer */}
       <span className="absolute inset-0 bg-green-500 transition-all duration-400 transform -translate-x-full group-hover:translate-x-0"></span>
 
@@ -8,13 +8,6 @@ export default function AnimatedButton() {
       <span className="relative text-[12px] text-[#39393b] font-medium z-10 transition-colors duration-300 group-hover:text-white">
         Read our mission
       </span>
-
-      {/* Border change on hover */}
-      <style jsx>{`
-        button:hover {
-          border-color: #22c55e; /* This is Tailwind's green-500 hex color */
-        }
-      `}</style>
     </button>
   );
 }
