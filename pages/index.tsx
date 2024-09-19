a0bdd61790a3d725a0b517da2f686ab47718afc2
Oludowole Olumide <xeonncodes@gmail.com>
2024-09-19 19:17:07 +0100

    Updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import DefaultLayout from "@/layouts/default";
 import { SiteFiles } from "@/config/siteFiles";
 import AnimatedButton from "@/components/animatedButton";
+import Link from "next/link";
 
 export default function IndexPage() {
   return (
@@ -22,7 +23,7 @@ export default function IndexPage() {
 
         {/* Right div with text content */}
         <div className="flex-1 bg-white flex items-center">
-          <div className="px-5 md:pl-[95px] mt-[0px] md:mt-[0px] lg:mt-[100px] w-full md:max-w-[78%] lg:max-w-[85%] xl:max-w-[78%]">
+          <div className="px-5 md:pl-[95px] mt-[-100px] md:mt-[0px] lg:mt-[100px] w-full md:max-w-[78%] lg:max-w-[85%] xl:max-w-[78%]">
             <h1 className="font-lora home-text-color text-[24px] xl:text-[34px] font-normal leading-[30px] xl:leading-[44px]">
               The future is built, <br />
               not wished or hoped for.
@@ -40,7 +41,9 @@ export default function IndexPage() {
             </p>
 
             <div className="mt-8">
-              <AnimatedButton />
+              <Link href="/mission">
+                <AnimatedButton />
+              </Link>
             </div>
           </div>
         </div>
