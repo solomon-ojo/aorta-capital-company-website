b3c37e37202b7ce8409076bc9568a7a8a00f3844
Oludowole Olumide <xeonncodes@gmail.com>
2024-09-19 15:57:10 +0100

    Updated

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,7 +1,6 @@
 import { Link } from "@nextui-org/link";
 
 import { Head } from "./head";
-
 import { Navbar } from "@/components/navbar";
 import { siteConfig } from "@/config/site";
 
@@ -11,8 +10,9 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex flex-col footer-bg h-[100svh]">
+    <div className="relative flex flex-col footer-bg h-[100svh] overflow-hidden">
       <Head />
+      {/* Navbar */}
       <div
         style={{ backgroundColor: "transparent" }}
         className="absolute top-0 z-30 w-full h-[65px] px-4 flex flex-col items-center justify-center"
@@ -20,7 +20,12 @@ export default function DefaultLayout({
         <Navbar />
       </div>
 
-      <main className="mx-auto w-full flex-grow">{children}</main>
+      {/* Main Content */}
+      <main className="mx-auto w-full flex-grow flex items-center justify-center">
+        {children}
+      </main>
+
+      {/* Footer */}
       <footer className="w-full footer-bg flex items-center justify-center h-[50px] lg:h-[100px]">
         <p className="footer-text font-lato">© 2024 {siteConfig.name}</p>
       </footer>
