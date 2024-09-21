435b3a51b6e7ab40dbf8f0b7269faad64c44bb6f
Oludowole Olumide <xeonncodes@gmail.com>
2024-09-21 05:58:04 +0100

    Updated

--- pages/our-companies/[company].tsx
+++ pages/our-companies/[company].tsx
@@ -15,11 +15,9 @@ export default function CompanyDetails() {
   const [openState, setOpenState] = useState(false);
 
   // Find the company details by name
-  const companyDetails = companies.find(
-    (c) => c.name.toLowerCase() === company
-  );
+  const companyObject = companies.find((c) => c.name.toLowerCase() === company);
 
-  if (!companyDetails) {
+  if (!companyObject) {
     return <p>Company not found</p>;
   }
 
@@ -27,7 +25,7 @@ export default function CompanyDetails() {
   const toggleMenu = () => {};
 
   return (
-    <div className="px-6 lg:px-11 bg-white flex flex-col gap-16 lg:gap-16 h-screen">
+    <div className="px-6 lg:px-11 bg-white flex flex-col gap-16 lg:gap-16 h-[100svh]">
       {/* Navbar */}
       <div className="sticky top-0 bg-white z-20">
         <NavbarDynamic openState={openState} toggleMenu={toggleMenu} />
@@ -39,17 +37,17 @@ export default function CompanyDetails() {
         <div className="w-full pr-0 lg:pr-6 lg:w-1/2 flex flex-col gap-10">
           {/* Company Logo */}
           <div>
-            <Link href={companyDetails.url}>
+            <Link href={companyObject.url}>
               <Image
-                src={companyDetails.logo}
-                alt={`${companyDetails.name} logo`}
+                src={companyObject.logo}
+                alt={`${companyObject.name} logo`}
                 width={200}
                 height={200}
               />
             </Link>
           </div>
           <p className="font-lora text-black  text-xl md:text-2xl lg:text-3xl xl:text-[33px] lg:leading-[40px] xl:leading-[50px] font-medium">
-            {companyDetails.details}
+            {companyObject.details}
           </p>
 
           {/* Connect */}
@@ -57,7 +55,7 @@ export default function CompanyDetails() {
             <span className="h-[1px] bg-gray-500 w-[70px] block"></span>
             <div className="my-5 flex items-center gap-4">
               <button className="h-[45px] text-white rounded-full text-sm transition-all duration-350 hover:text-black border border-black hover:bg-transparent bg-black px-[30px]">
-                {companyDetails.url}
+                {companyObject.url}
               </button>
               <div className="h-[45px] w-[45px] flex items-center justify-center overflow-hidden border border-black hover:bg-black rounded-full"></div>
               <div className="h-[45px] w-[45px] flex items-center justify-center overflow-hidden border border-black hover:bg-black rounded-full"></div>
