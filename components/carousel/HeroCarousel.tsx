9078847110d03135f0e45676d0115d87ddc4ccd8
Oludowole Olumide <xeonncodes@gmail.com>
2024-09-28 14:07:01 +0100

    Added carousel

---
+++ components/carousel/HeroCarousel.tsx
@@ -0,0 +1,71 @@
+import React, { useCallback, forwardRef, useImperativeHandle } from "react";
+import { EmblaOptionsType } from "embla-carousel";
+import Image from "next/image";
+import useEmblaCarousel from "embla-carousel-react";
+
+type PropType = {
+  // options?: EmblaOptionsType;
+  slide: number;
+};
+
+const HeroCarousel = forwardRef((props, ref) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    loop: true,
+    align: "center",
+  });
+
+  const scrollTo = (num: number) => {
+    emblaApi?.scrollTo(num);
+  };
+
+  useImperativeHandle(ref, () => ({
+    scrollTo,
+  }));
+
+  const scrollTos = useCallback(
+    (num: number) => {
+      emblaApi?.scrollTo(num);
+    },
+    [emblaApi]
+  );
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        <div className="embla__slide relative border-t-[11px] border-[#f8f0d9] flex-shrink-0 px-5 flex-grow-0 basis-[88%] md:basis-[750px] lg:basis-[800px] xl:basis-[900px] shadow-2xl bg-white mx-[120px] h-[550px] ">
+          <Image
+            quality={40}
+            src={"/assets/woman.png"}
+            alt="test-img"
+            objectFit="cover"
+            layout="fill"
+          />
+        </div>
+        <div className="embla__slide relative border-t-[11px] border-[#f8f0d9] flex-shrink-0 px-5 flex-grow-0 basis-[88%] md:basis-[750px] lg:basis-[800px] xl:basis-[900px] shadow-2xl bg-white mx-[120px] h-[550px] ">
+          <Image
+            quality={40}
+            src={"/assets/testheroimg.webp"}
+            alt="test-img"
+            objectFit="cover"
+            layout="fill"
+          />
+        </div>
+        <div className="embla__slide relative border-t-[11px] border-[#f8f0d9] flex-shrink-0 px-5 flex-grow-0 basis-[88%] md:basis-[750px] lg:basis-[800px] xl:basis-[900px] shadow-2xl bg-white mx-[120px] h-[550px] ">
+          <Image
+            quality={40}
+            src={"/assets/testimgimg.jpg"}
+            alt="test-img"
+            objectFit="cover"
+            layout="fill"
+          />
+        </div>
+      </div>
+    </div>
+  );
+});
+
+export default HeroCarousel;
