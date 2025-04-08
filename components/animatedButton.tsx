b4e0108038dbab356d6d37bfb888bd41c19b7153
Wole Ojo <woleojo@MacBookAir.fritz.box>
2025-04-08 04:16:57 +0200

    chaned margin

--- components/animatedButton.tsx
+++ components/animatedButton.tsx
@@ -1,11 +1,11 @@
 export default function AnimatedButton() {
   return (
-    <button className="relative flex items-center justify-center home-button group border-1 border-[#6E6F7A] w-full text-black md:w-[160px] h-[45px] font-semibold overflow-hidden transition-all duration-300">
+    <button className="relative flex items-center justify-center bg-[#22c55e] md:bg-white home-button group border-1 border-[#6E6F7A] w-full text-black md:w-[160px] h-[45px] font-semibold overflow-hidden transition-all duration-300">
       {/* Background layer */}
       <span className="absolute inset-0 bg-[#22c55e] transition-all duration-400 transform -translate-x-full group-hover:translate-x-0"></span>
 
       {/* Text layer */}
-      <span className="relative font-lora text-[12px] text-[#39393b] font-medium z-10 transition-colors duration-300 group-hover:text-white">
+      <span className="relative font-lora text-[12px] text-white md:text-[#39393b] font-medium z-10 transition-colors duration-300 group-hover:text-white">
         Read our mission
       </span>
     </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function IndexPage() {
     <DefaultLayout>
       <section className="flex h-full flex-col lg:flex-row">
         {/* Left div with background image */}
-        <div className="lg:flex-1 h-[130px] md:h-[300px] lg:h-full relative">
+        <div className="lg:flex-1 h-[200px] md:h-[300px] lg:h-full relative">
           <Image
             src={SiteFiles.homeBg}
             alt="Background Image"
@@ -23,7 +23,7 @@ export default function IndexPage() {
 
         {/* Right div with text content */}
         <div className="flex-1 bg-white flex items-center">
-          <div className="px-6 md:pl-[95px] mt-[-100px] md:mt-[0px] lg:mt-[100px] w-full md:max-w-[78%] lg:max-w-[85%] xl:max-w-[78%]">
+          <div className="px-6 md:pl-[95px] mt-[55px] lg:mt-[100px] w-full md:max-w-[78%] lg:max-w-[85%] xl:max-w-[78%]">
             <h1 className="font-lora home-text-color text-[24px] xl:text-[34px] font-normal leading-[30px] xl:leading-[44px]">
               The future is built <br /> not hoped for.
             </h1>
