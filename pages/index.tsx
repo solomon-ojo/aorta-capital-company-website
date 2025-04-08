c4916fd8399e945e529b0a5d4359a36a01d1300a
Wole Ojo <woleojo@MacBookAir.fritz.box>
2025-04-08 04:44:27 +0200

    chaned margin

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,11 +24,11 @@ export default function IndexPage() {
         {/* Right div with text content */}
         <div className="flex-1 bg-white flex items-center">
           <div className="px-6 md:pl-[95px] mt-[55px] mb-[40px] lg:mb-[0px]  lg:mt-[100px] w-full md:max-w-[78%] lg:max-w-[85%] xl:max-w-[78%]">
-            <h1 className="font-lora home-text-color text-[24px] xl:text-[34px] font-normal leading-[30px] xl:leading-[44px]">
+            <h1 className="font-lora  home-text-color text-[24px] xl:text-[34px] font-normal leading-[30px] xl:leading-[44px]">
               The future is built <br /> not hoped for.
             </h1>
             <span className="h-[2px] bg-black w-4 block my-6 "></span>
-            <p className="font-lora opacity-85 leading-[25px] home-text-color font-light text-[17px]">
+            <p className="font-lora italic opacity-85 leading-[25px] home-text-color font-light text-[17px]">
               We partner with bold, visionary entrepreneurs to build the Africa
               we desire—by Africans, for Africans—unhindered by government
               inefficiencies. Together, we harness the limitless potential of
