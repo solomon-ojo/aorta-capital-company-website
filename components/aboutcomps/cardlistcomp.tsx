431aa7392adb339cb5463b0999a79f8ec0c3824e
Wole Ojo <woleojo@MacBookAir.fritz.box>
2025-04-08 02:43:00 +0200

    add

--- components/aboutcomps/cardlistcomp.tsx
+++ components/aboutcomps/cardlistcomp.tsx
@@ -5,12 +5,14 @@ type Types = {
   description: string;
 };
 
+const Icon = IoMdCheckmarkCircleOutline as React.FC<React.SVGProps<SVGSVGElement>>;
+
 export const CardListComp = ({ title, description }: Types) => {
   return (
     <div className="flex items-start gap-2 sm:gap-4">
       {/* icon */}
       <div className="h-10 w-10 shrink-0 flex items-center justify-center rounded-full about-hero-light-yellow-bg">
-        <IoMdCheckmarkCircleOutline size={24} color="#0425e8" />
+        <Icon width={24} height={24} color="#0425e8" />
       </div> 
       <div className="flex flex-col gap-1">
         <h1 className="text-[16px] sm:text-[20px] font-semibold">{title}</h1>
